47fc66073008b2fba3e815b5b728adf1a3ead89c
NStephenson <nickstephenson90@gmail.com>
2016-11-17 22:21:02 -0500

    added merge sort

--- alphabetize.js
+++ alphabetize.js
@@ -1,3 +1,5 @@
+const suffixList = ["jr.", "jr", "ii", "i", "iii", "sr.", "sr"]
+
 function sortName(name, option){
   if(option === "first"){
     return name.toLowerCase()
@@ -21,6 +23,36 @@ function shiftLastName(name){
   return splitNames[lastNameIndex] + " " + splitNames[0];
 }
 
-const suffixList = ["jr.", "jr", "ii", "i", "iii", "sr.", "sr"]
-// Nicholas Stephenson
-// Martin Luther King Jr.
+function sortNamesList(namesList, options){
+  return mergeSort(namesList, options)
+}
+
+function merge(left, right, options){
+    var result  = [],
+        il      = 0,
+        ir      = 0;
+
+    while (il < left.length && ir < right.length){
+        if (sortName(left[il], options.nameUsed) < sortName(right[ir], options.nameUsed)){
+            result.push(options.reversed ? right[ir++] : left[il++]);
+            
+        } else {
+            result.push(options.reversed ? left[il++] : right[ir++]);
+        }
+    }
+
+    return result.concat(left.slice(il)).concat(right.slice(ir));
+}
+
+function mergeSort(items, options){
+
+    if (items.length < 2) {
+        return items;
+    }
+
+    var middle = Math.floor(items.length / 2),
+        left    = items.slice(0, middle),
+        right   = items.slice(middle);
+
+    return merge(mergeSort(left, options), mergeSort(right, options), options);
+}
